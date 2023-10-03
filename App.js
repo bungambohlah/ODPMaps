2a72b54997b614313af7acfd5731bc3c46fbdf1f
Afif Abdillah Jusuf <bungambohlah@gmail.com>
2023-10-03 21:32:37 +0700

    feat: :sparkles: initiazlie project

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useState, useEffect} from 'react'
+import { StyleSheet, View, Text } from 'react-native';
+import Mapbox from 'rnmapbox/maps';
 
+Mapbox.setAccessToken('pk.eyJ1IjoiYnVuZ2FtYm9obGFoIiwiYSI6ImNsbjkzOHc1dDAzNm4ya253aXh6a2tjbG8ifQ.iwEYr3cMfHciuU4LUuu9aw');
 export default function App() {
+  const [mb, setMb] = useState(null)
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+   <View style={styles.page}>
+      <View style={styles.container}>
+        {mb ? (<mb.MapView style={styles.map} />) : null}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  page: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
+  container: {
+    height: 300,
+    width: 300,
+  },
+  map: {
+    flex: 1
+  }
 });
+
