15f9a95dfff70350489f5c61356bb750b7cf6f22
Afif Abdillah Jusuf <bungambohlah@gmail.com>
2023-10-12 22:29:24 +0700

    feat: :sparkles: implement new feature to integrate upstash with location points

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef, Fragment } from "react";
 import { StatusBar } from "expo-status-bar";
 import { View, Text, StyleSheet } from "react-native";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
@@ -8,12 +8,70 @@ import FAB from "./src/components/FAB";
 Mapbox.setAccessToken(
   "pk.eyJ1IjoiYnVuZ2FtYm9obGFoIiwiYSI6ImNsbjkzOHc1dDAzNm4ya253aXh6a2tjbG8ifQ.iwEYr3cMfHciuU4LUuu9aw",
 );
+const UPSTASH_URL = "https://apn1-frank-cowbird-33009.upstash.io";
+const UPSTASH_TOKEN =
+  "AYDxASQgOWMxZjMwMWItODJkMS00MTg1LTliYjgtOTA5ZGU0OTE0NjA2ODVhODA3MWNhY2Q4NGRmZDhmNDI0NGRkMjViOWYxM2I=";
 const DEFAULT_CENTER_COORDINATE = [112.74795609717692, -7.263394274153487];
 
 const App = () => {
   const [location, setLocation] = useState(null);
   const [permission, setPermission] = useState(false);
+  const [annotations, setAnnotations] = useState({});
+  const [annotationsLoading, setAnnotationsLoading] = useState(false);
   const camera = useRef(null);
+
+  const setAnnotationNamesAPI = async () => {
+    await fetch(`${UPSTASH_URL}/set/locationNames/["ODP1"]`, {
+      headers: {
+        Authorization: `Bearer ${UPSTASH_TOKEN}`,
+      },
+    });
+  };
+  const getAnnotationNamesAPI = async () => {
+    const response = await fetch(`${UPSTASH_URL}/get/locationNames`, {
+      headers: {
+        Authorization: `Bearer ${UPSTASH_TOKEN}`,
+      },
+    });
+    const data = await response.json();
+    const locationNames = JSON.parse(data.result || "[]");
+    const reducedLocationNames = locationNames.reduce((obj, key) => {
+      obj[key] = [];
+      return obj;
+    }, {});
+    setAnnotations(reducedLocationNames);
+    return reducedLocationNames;
+  };
+  const setAnnotationsAPI = async (reducedLocationNames) => {
+    for (const [key, value] of Object.entries(reducedLocationNames)) {
+      // TODO: value it will be coordinate that need to set
+      await fetch(
+        `${UPSTASH_URL}/set/${key}/${JSON.stringify([
+          location?.coords?.longitude,
+          location?.coords?.latitude,
+        ])}`,
+        {
+          headers: {
+            Authorization: `Bearer ${UPSTASH_TOKEN}`,
+          },
+        },
+      );
+    }
+
+    return reducedLocationNames;
+  };
+  const getAnnotationsAPI = async (reducedLocationNames) => {
+    for (const [key, value] of Object.entries(reducedLocationNames)) {
+      const response = await fetch(`${UPSTASH_URL}/get/${key}`, {
+        headers: {
+          Authorization: `Bearer ${UPSTASH_TOKEN}`,
+        },
+      });
+      const data = await response.json();
+      const coords = JSON.parse(data.result || "[]");
+      setAnnotations((s) => ({ ...s, [key]: coords }));
+    }
+  };
 
   useEffect(() => {
     Mapbox.setTelemetryEnabled(false);
@@ -30,7 +88,16 @@ const App = () => {
       centerCoordinate: [location?.coords?.longitude, location?.coords?.latitude],
       zoomLevel: 16,
     });
-  }, [location]);
+
+    if (!annotationsLoading && location) {
+      setAnnotationsLoading(true);
+      setAnnotationNamesAPI()
+        .then(() => getAnnotationNamesAPI())
+        .then((reducedLocationNames) => setAnnotationsAPI(reducedLocationNames))
+        .then((reducedLocationNames) => getAnnotationsAPI(reducedLocationNames))
+        .catch((err) => console.error(err));
+    }
+  }, [location, annotationsLoading]);
 
   return (
     <View className="flex justify-center items-center">
@@ -49,6 +116,19 @@ const App = () => {
                 centerCoordinate={DEFAULT_CENTER_COORDINATE}
                 zoomLevel={16}
               />
+              {Object.entries(annotations)?.map(([name, coords], idx) => (
+                <Fragment key={idx}>
+                  {Array.isArray(coords) && coords.length ? (
+                    <Mapbox.PointAnnotation
+                      key={idx}
+                      id={`odp-${idx}`}
+                      coordinate={coords}
+                      title={name}
+                      selected
+                    />
+                  ) : null}
+                </Fragment>
+              ))}
             </Mapbox.MapView>
             <FAB
               onPress={() => {
