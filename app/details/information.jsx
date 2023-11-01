84ff4a2e84ed5567444121fdfd5fcf20cc1cf1ce
Afif Abdillah Jusuf <bungambohlah@gmail.com>
2023-11-02 00:42:41 +0700

    refactor: :necktie: add some enhancements for ODP information page
    * add new implementation to edit ODP name
    * move snackbar into information page
    * fix wrong route parameters between stack pages

--- app/details/information.jsx
+++ app/details/information.jsx
@@ -1,17 +1,19 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { useLocalSearchParams } from "expo-router";
+import { router, useGlobalSearchParams } from "expo-router";
 import { RefreshControl, ScrollView, Text, View } from "react-native";
-import { ActivityIndicator, Card } from "react-native-paper";
+import { ActivityIndicator, Card, Snackbar } from "react-native-paper";
 
 const UPSTASH_URL = "https://apn1-frank-cowbird-33009.upstash.io";
 const UPSTASH_TOKEN =
   "AYDxASQgOWMxZjMwMWItODJkMS00MTg1LTliYjgtOTA5ZGU0OTE0NjA2ODVhODA3MWNhY2Q4NGRmZDhmNDI0NGRkMjViOWYxM2I=";
 
 function Page() {
-  const { name } = useLocalSearchParams();
+  const { name, snackMessage } = useGlobalSearchParams();
   const [information, setInformation] = useState(null);
   const [loading, setLoading] = useState(false);
   const [refreshing, setRefreshing] = React.useState(false);
+
+  const onDismissSnackBar = () => router.setParams({ snackMessage: "" });
 
   const onRefresh = useCallback(() => {
     setRefreshing(true);
@@ -20,44 +22,52 @@ function Page() {
       setLoading(false);
       setRefreshing(false);
     });
-  }, []);
+  }, [name]);
 
   async function getInformationODP() {
-    const response = await fetch(`${UPSTASH_URL}/get/${name}-Information`, {
-      headers: {
-        Authorization: `Bearer ${UPSTASH_TOKEN}`,
-      },
-    });
-    const data = await response.json();
-    const resultData = JSON.parse(data.result || "");
-    const formattedData = resultData.replaceAll("<enter>", "\n");
-    setInformation(formattedData);
+    try {
+      if (name) {
+        const response = await fetch(`${UPSTASH_URL}/get/${name}-Information`, {
+          headers: {
+            Authorization: `Bearer ${UPSTASH_TOKEN}`,
+          },
+        });
+        const data = await response.json();
+        const resultData = JSON.parse(data.result || "");
+        const formattedData = resultData.replaceAll("<enter>", "\n");
+        setInformation(formattedData);
+      }
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   useEffect(() => {
-    if (!information) {
-      setLoading(true);
-      getInformationODP().finally(() => setLoading(false));
-    }
-  }, [information]);
+    onRefresh();
+  }, [information, name, onRefresh]);
 
   return (
-    <ScrollView refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
-      <Card className="p-4 m-4">
-        {!loading ? (
-          <>
-            <Text className="text-xl font-bold">{name}</Text>
-            <View className="mt-4">
-              <Text className={`text-sm ${!information?.length ? "italic" : ""}`}>
-                {information?.length ? information : "No description"}
-              </Text>
-            </View>
-          </>
-        ) : (
-          <ActivityIndicator animating={true} />
-        )}
-      </Card>
-    </ScrollView>
+    <>
+      <ScrollView refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+        <Card className="p-4 m-4">
+          {!loading ? (
+            <>
+              <Text className="text-xl font-bold">{name}</Text>
+              <View className="mt-4">
+                <Text className={`text-sm ${!information?.length ? "italic" : ""}`}>
+                  {information?.length ? information : "No description"}
+                </Text>
+              </View>
+            </>
+          ) : (
+            <ActivityIndicator animating={true} />
+          )}
+        </Card>
+      </ScrollView>
+      <Snackbar visible={snackMessage?.length || false} onDismiss={onDismissSnackBar}>
+        {snackMessage}
+      </Snackbar>
+    </>
   );
 }
 
--- app/edit/edit-information.jsx
+++ app/edit/edit-information.jsx
@@ -1,66 +1,110 @@
-import React, { useState } from "react";
-import { useLocalSearchParams } from "expo-router";
+import React, { useEffect, useState } from "react";
+import { router, useLocalSearchParams } from "expo-router";
 import { Text, View } from "react-native";
-import { Button, Card, Snackbar, TextInput } from "react-native-paper";
+import { ActivityIndicator, Button, Card, TextInput } from "react-native-paper";
 
 const UPSTASH_URL = "https://apn1-frank-cowbird-33009.upstash.io";
 const UPSTASH_TOKEN =
   "AYDxASQgOWMxZjMwMWItODJkMS00MTg1LTliYjgtOTA5ZGU0OTE0NjA2ODVhODA3MWNhY2Q4NGRmZDhmNDI0NGRkMjViOWYxM2I=";
+const opt = { headers: { Authorization: `Bearer ${UPSTASH_TOKEN}` } };
 
 function Page() {
   const { name } = useLocalSearchParams();
   const [information, setInformation] = useState("");
-  const [visible, setVisible] = useState(false);
-  const [snackMessage, setSnackMessage] = useState("");
+  const [odpName, setODPName] = useState("");
   const [disabled, setDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const onDismissSnackBar = () => setVisible(false);
+  async function getInformationODP() {
+    setLoading(true);
+    try {
+      const response = await fetch(`${UPSTASH_URL}/get/${name}-Information`, opt);
+      const data = await response.json();
+      const resultData = JSON.parse(data.result || null);
+      setInformation(resultData);
+      setODPName(name);
+    } catch (error) {
+      setInformation("");
+      setODPName("");
+    } finally {
+      setLoading(false);
+    }
+  }
 
-  async function setInformationODP(desc) {
-    const formattedDesc = JSON.stringify(desc).replaceAll("\\n", "<enter>");
-    console.log(formattedDesc);
-    await fetch(`${UPSTASH_URL}/set/${name}-Information/${formattedDesc}`, {
-      headers: {
-        Authorization: `Bearer ${UPSTASH_TOKEN}`,
-      },
-    });
+  async function setInformationODP(desc, odpName) {
+    try {
+      desc = desc ? desc : "";
+      const formattedDesc = JSON.stringify(desc).replaceAll("\\n", "<enter>");
+
+      // update information
+      await fetch(`${UPSTASH_URL}/del/${name}-Information`, opt);
+      await fetch(`${UPSTASH_URL}/set/${odpName}-Information/${formattedDesc}`, opt);
+
+      // update coordinates location name
+      const response = await fetch(`${UPSTASH_URL}/get/${name}`, opt);
+      const data = await response.json();
+      const currentCoord = JSON.parse(data.result || null);
+      await fetch(`${UPSTASH_URL}/set/${odpName}/${JSON.stringify(currentCoord)}`, opt);
+      await fetch(`${UPSTASH_URL}/del/${name}`, opt);
+
+      // update list of locations
+      const responseLoc = await fetch(`${UPSTASH_URL}/get/locationNames`, opt);
+      const dataLoc = await responseLoc.json();
+      let locationNames = JSON.parse(dataLoc.result || null);
+      locationNames = locationNames.filter((x) => x !== name);
+      locationNames.push(odpName);
+      await fetch(`${UPSTASH_URL}/set/locationNames/${JSON.stringify(locationNames)}`, opt);
+
+      router.setParams({ name: odpName });
+      router.back();
+      router.replace({ pathname: "/details/information", params: { name: odpName } });
+    } catch (error) {
+      // console.error(error);
+    }
   }
 
   function handleSubmit() {
     setDisabled(true);
-    setInformationODP(information)
-      .then(() => {
-        setSnackMessage("Successfully to update description.");
-        setVisible(true);
-      })
-      .catch(() => {
-        setSnackMessage("Failed to update description.");
-        setVisible(true);
-      })
+    setInformationODP(information, odpName)
+      .then(() => router.setParams({ snackMessage: "Successfully to update description." }))
+      .catch(() => router.setParams({ snackMessage: "Failed to update description." }))
       .finally(() => {
         setDisabled(false);
       });
   }
 
+  useEffect(() => {
+    // load first time
+    getInformationODP();
+  }, [name]);
+
   return (
     <>
       <Card className="p-4 m-4">
-        <Text className="text-xl font-bold">Edit Information {name}</Text>
-        <View className="flex flex-col gap-4 mt-4">
-          <TextInput
-            label="Description"
-            value={information}
-            onChangeText={(text) => setInformation(text)}
-            multiline
-          />
-          <Button mode="contained" onPress={handleSubmit} disabled={disabled}>
-            Submit
-          </Button>
-        </View>
+        {!loading ? (
+          <>
+            <Text className="text-xl font-bold">Edit Information {name}</Text>
+            <View className="flex flex-col gap-4 mt-4">
+              <TextInput
+                label="ODP Name"
+                value={odpName}
+                onChangeText={(text) => setODPName(text)}
+              />
+              <TextInput
+                label="Description"
+                value={information}
+                onChangeText={(text) => setInformation(text)}
+                multiline
+              />
+              <Button mode="contained" onPress={handleSubmit} disabled={disabled}>
+                Submit
+              </Button>
+            </View>
+          </>
+        ) : (
+          <ActivityIndicator animating={true} />
+        )}
       </Card>
-      <Snackbar visible={visible} onDismiss={onDismissSnackBar}>
-        {snackMessage}
-      </Snackbar>
     </>
   );
 }
